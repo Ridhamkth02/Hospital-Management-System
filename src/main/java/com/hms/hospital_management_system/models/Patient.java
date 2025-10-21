5fd1ad2741c23e87ff7dc209e9f5687c2be7326a
Ridham Gupta <Ridhamkth@gmail.com>
2025-10-22 01:46:30 +0530

    Setup application.properties and add entity models for Patient, Doctor, Appointment, and Bill

---
+++ src/main/java/com/hms/hospital_management_system/models/Patient.java
@@ -0,0 +1,24 @@
+package com.hms.hospital_management_system.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String gender;
+    private int age;
+
+
+
+}
